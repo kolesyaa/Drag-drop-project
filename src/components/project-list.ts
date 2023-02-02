037b742e91cb19071e0bcc10b537269a6463e2ee
kolesyaa <kolesyaaa@outlook.com>
2023-02-02 23:36:30 +0800

    added webpack config

--- src/components/project-list.ts
+++ src/components/project-list.ts
@@ -1,9 +1,9 @@
-import { Component } from "./base-component.js";
-import { Project, ProjectStatus } from "../models/project.js";
-import { autobind } from "../decorators/autobind.js";
-import { projectState } from "../state/project-state.js";
-import { DragTarget } from "../models/drag-drop.js";
-import { ProjectItem } from "./project-item.js";
+import { Component } from "./base-component";
+import { Project, ProjectStatus } from "../models/project";
+import { autobind } from "../decorators/autobind";
+import { projectState } from "../state/project-state";
+import { DragTarget } from "../models/drag-drop";
+import { ProjectItem } from "./project-item";
 
 // Project list class
 export class ProjectList
